4b6b83fb18619416b8e14ce1f86c3cdec92eb692
Swapnil Shinde <me.swapnilshinde44@gmail.com>
2024-01-06 22:29:42 +0530

    Yo

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
-// db.js
 const mongoose = require("mongoose");
 
-// const password = "fhioQ3bpMkUhrvju";
-// const MONGO_URL = `mongodb+srv://Mario44:${password}@cluster0.zasthev.mongodb.net/gofood_mern?retryWrites=true&w=majority`;
-const username = "Mario44"
+const username = "Mario44";
 const password = "fhioQ3bpMkUhrvju";
 const clusterName = "ac-kfirdtb";
 const databaseName = "gofood_mern";
@@ -19,14 +16,15 @@ const mongoDB = async () => {
 
     const fetched_data_cursor = mongoose.connection.db.collection("Sample").find({});
     const data = await fetched_data_cursor.toArray();
-    // console.log('Data fetched:', data);
+    console.log('Data fetched:', data);
 
     const foodCategory_cursor = mongoose.connection.db.collection("Category").find({});
     const catData = await foodCategory_cursor.toArray();
-    // console.log('Categories fetched:', catData);
+    console.log('Categories fetched:', catData);
 
-    global.food_items = data;
-    global.foodCategory = catData;
+    // Store the data in variables or process it further as needed
+    // global.food_items = data;
+    // global.foodCategory = catData;
 
   } catch (error) {
     console.error("Error connecting to MongoDB :( \n:", error);
