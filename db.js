1aa45120246d0407768e61162038ab4f4a604ae9
Swapnil Shinde <me.swapnilshinde44@gmail.com>
2024-01-06 22:32:02 +0530

    Yo

--- db.js
+++ db.js
@@ -14,17 +14,15 @@ const mongoDB = async () => {
     await mongoose.connect(MONGO_URL, { useNewUrlParser: true });
     console.log("Connected to DB :)");
 
-    const fetched_data_cursor = mongoose.connection.db.collection("Sample").find({});
-    const data = await fetched_data_cursor.toArray();
-    console.log('Data fetched:', data);
+    const fetchedData = await mongoose.connection.db.collection("Sample").find({}).toArray();
+    console.log('Data fetched:', fetchedData);
 
-    const foodCategory_cursor = mongoose.connection.db.collection("Category").find({});
-    const catData = await foodCategory_cursor.toArray();
-    console.log('Categories fetched:', catData);
+    const categoryData = await mongoose.connection.db.collection("Category").find({}).toArray();
+    console.log('Categories fetched:', categoryData);
 
     // Store the data in variables or process it further as needed
-    // global.food_items = data;
-    // global.foodCategory = catData;
+    // global.food_items = fetchedData;
+    // global.foodCategory = categoryData;
 
   } catch (error) {
     console.error("Error connecting to MongoDB :( \n:", error);
