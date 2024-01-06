d316239c80b208559ef314e7191a957e0496bb1f
Swapnil Shinde <me.swapnilshinde44@gmail.com>
2024-01-06 18:51:56 +0530

    Yo

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const CreateUser = require("./Routes/CreateUser")
 const DisplayData = require('./Routes/DisplayData')
 const orderData = require('./Routes/OrderData')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({"global" :"global.food_items"})
 })
 app.use(express.json())
 app.use(cors());
