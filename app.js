dd296655b77f1c44b5653ae9da3a0e914924dde8
Swapnil Shinde <me.swapnilshinde44@gmail.com>
2024-01-06 18:47:21 +0530

    Yo

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+// app.js
+const express = require('express')
+const app = express()
+const port = 5000
 const cors = require('cors');
-const { connectToDB, router } = require('./db');
-
-app.use(express.json());
-app.use(cors());
+const DB = require('./db');
+DB(); 
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept"
+  );
+  next();
+})
 
 // Routes
-const CreateUser = require("./Routes/CreateUser");
-const orderData = require('./Routes/OrderData');
-
+const CreateUser = require("./Routes/CreateUser")
+const DisplayData = require('./Routes/DisplayData')
+const orderData = require('./Routes/OrderData')
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-connectToDB()
-  .then(() => {
-    app.use('/api', CreateUser);
-    app.use('/api', router); // Use the router from db.js for /foodData route
-    app.use('/api', orderData);
-
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch(error => {
-    console.error('Failed to start the server:', error);
-  });
+  res.send('Hello World!')
+})
+app.use(express.json())
+app.use(cors());
+app.use('/api',CreateUser)
+app.use('/api',DisplayData)
+app.use('/api',orderData)
+app.listen(port, () => {
+  
+  console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
