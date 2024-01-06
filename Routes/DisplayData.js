f17e35314313681e1f862ecd1303573b49929770
Swapnil Shinde <me.swapnilshinde44@gmail.com>
2024-01-06 16:11:54 +0530

    global var update

--- Routes/DisplayData.js
+++ Routes/DisplayData.js
@@ -1,20 +1,21 @@
+// DisplayData.js
 const express = require("express");
 const router = express.Router();
 
 router.get('/foodData', (req, res) => {
     try {
-        console.log(global.food_items)
-        const { food_items, foodCategory } = global;
-        const responseData = [food_items,foodCategory]
+        console.log("Global Food Items:", global.food_items);
+        console.log("Global Food Category:", global.foodCategory);
 
-        console.log(responseData);
+        const { food_items, foodCategory } = global;
+        const responseData = [food_items, foodCategory];
+
+        console.log("Response Data:", responseData);
         res.send(responseData);
     } catch (err) {
-        console.log(err.message);
+        console.log("Error:", err.message);
         res.status(500).send('Server Error :(');
     }
 });
 
 module.exports = router;
-
-
--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 const cors = require('cors');
 const DB = require('./db');
-DB(); 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept"
-  );
-  next();
-})
+
+app.use(express.json());
+app.use(cors());
 
 // Routes
-const CreateUser = require("./Routes/CreateUser")
-const DisplayData = require('./Routes/DisplayData')
-const orderData = require('./Routes/OrderData')
+const CreateUser = require("./Routes/CreateUser");
+const DisplayData = require('./Routes/DisplayData');
+const orderData = require('./Routes/OrderData');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.use(express.json())
-app.use(cors());
-app.use('/api',CreateUser)
-app.use('/api',DisplayData)
-app.use('/api',orderData)
-app.listen(port, () => {
-  
-  console.log(`Example app listening on port ${port}`)
-})
+  res.send('Hello World!');
+});
+
+DB().then(() => {
+  // Start server after DB connection and data fetch
+  app.use('/api', CreateUser);
+  app.use('/api', DisplayData);
+  app.use('/api', orderData);
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}).catch(error => {
+  console.error('Failed to start the server:', error);
+});
 
 module.exports = app;
